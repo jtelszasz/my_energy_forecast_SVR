c0b319a4187fce678f67214fbe94db9bd74f82fa
Justin Elszasz <jtelszasz@gmail.com>
2014-04-10 21:16:57 -0400

    added March data

--- import_funcs.py
+++ import_funcs.py
@@ -1,14 +1,33 @@
+"""
+import_funcs.py class has two functions - weather() and BGEdata()
+
+
+weather() aggregates and returns a Pandas dataframe with hourly weather readings. The csv files
+are hourly weather observations from weatherunderground.com using 'weather_download_observations.py'
+Add additional csv's using weather.append.
+params: none
+returns: 'weather' - Pandas dataframe, hourly weather data
+
+BGEdata() aggregates and returns a Pandas dataframe with hourly electricity usage from BGE.com.
+params: none
+returns: 'DF' - Pandas dataframe, hourly electricity usage
+
+
+Justin Elszasz 2014.04.10
+"""
+
 import sys
 import numpy as np
 import pandas as pd
 from datetime import datetime
 import time_parser
-#reload(time_parser)
 
 def weather():
 
-	weather = pd.read_csv('weather_20140101to20140131_df.csv',skiprows=0)
-	weather = weather.append(pd.read_csv('weather_20140201to20140228_df.csv',skiprows=0))
+	# Add additional weather csv's downloaded using 'weather_download_obeservations.py' here
+	weather = pd.read_csv('raw_data/weather_20140101to20140131_df.csv',skiprows=0)
+	weather = weather.append(pd.read_csv('raw_data/weather_20140201to20140228_df.csv',skiprows=0))
+	weather = weather.append(pd.read_csv('raw_data/weather_20140301to20140331_df.csv',skiprows=0))
 
 	weather['timestamp'] = weather.iloc[:,0]
 	weather.index = pd.to_datetime(weather['timestamp'])
@@ -28,10 +47,11 @@ def weather():
 
 def BGEdata():
 
-	DF = pd.read_csv('raw_data/DailyElectricUsage_201401.csv',skiprows=4,parse_dates={'timestamp':['DATE','START TIME'],'timestamp_end':['DATE','END TIME']},index_col='timestamp')
-	DF = DF.append(pd.read_csv('raw_data/DailyElectricUsage_201402.csv',skiprows=4,parse_dates={'timestamp':['DATE','START TIME'],'timestamp_end':['DATE','END TIME']},index_col='timestamp'))
+	# Add additional electricity usage csv's downloaded using Green Button protocol on BGE.com
+	elec = pd.read_csv('raw_data/DailyElectricUsage_201401.csv',skiprows=4,parse_dates={'timestamp':['DATE','START TIME'],'timestamp_end':['DATE','END TIME']},index_col='timestamp')
+	elec = elec.append(pd.read_csv('raw_data/DailyElectricUsage_201402.csv',skiprows=4,parse_dates={'timestamp':['DATE','START TIME'],'timestamp_end':['DATE','END TIME']},index_col='timestamp'))
+	elec = elec.append(pd.read_csv('raw_data/DailyElectricUsage_201403.csv',skiprows=5,parse_dates={'timestamp':['DATE','START TIME'],'timestamp_end':['DATE','END TIME']},index_col='timestamp'))
 
-
-	return DF
+	return elec
 
 
